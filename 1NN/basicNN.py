f4ea0e21d720001d7b2fb5252ffb8c30aed2c2d1
EmhRk <43347776+EmhRk@users.noreply.github.com>
2019-10-03 15:05:39 +0800

    try with C

---
+++ 1NN/basicNN.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+
+import numpy as np
+
+#  basic neural network
+
+#  parameters
+
+class BNN:
+    def __init__(self):
+        pass
+
+    def predict(self, x):
+        pass
+
+def basicNN():
+    bnn = BNN()
+    return bnn
