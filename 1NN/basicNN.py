0bb3c3f51fe199355cae20ef15553cf7c9d9679a
Emh <745981022@qq.com>
2019-10-03 16:20:48 +0800

    ready

--- 1NN/basicNN.py
+++ 1NN/basicNN.py
@@ -20,10 +20,10 @@ class Predictor:
 # dataList like [[data00,data01,data02,...],[data10,data11,data12,...],...]
 # learnRate usually in [0.01, 0.8]. an overSize learnRate will cause unstable learning process
 # tol is the quit condition of loop
-# numHL means number of hidden layers. -1 allow computer to make a decision itself
+# HLSize means number of hidden layers. -1 allow computer to make a decision itself
 
 class BasicNN:
-    def __init__(self, dataList, labelsList, learnRate=0.8, tol=0.05, maxIter=20, numHL=-1):
+    def __init__(self, dataList, labelsList, learnRate=0.8, tol=0.05, maxIter=20, HLSize=-1):
         #  type check
         if not isinstance(dataList, list):
             raise NameError('DataList should be list')
@@ -42,7 +42,13 @@ class BasicNN:
 
         self.inputSize = len(dataList[0][0])
 
+        # I m not sure whether this output size is suitable
+        self.outputSize = len(set(labelsList))
+
+        self.HLSize =
+
         dataMat = np.mat(dataList)
+
 
         for i in range(maxIter):
 
